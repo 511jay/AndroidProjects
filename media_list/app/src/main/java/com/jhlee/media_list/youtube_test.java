2a7df76e724b09e5a3912d4e6560a227aac9bd7d
511jay <511jay@naver.com>
2021-03-31 19:56:30 +0900

    uploads

---
+++ media_list/app/src/main/java/com/jhlee/media_list/youtube_test.java
@@ -0,0 +1,14 @@
+package com.jhlee.media_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class youtube_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_test);
+    }
+}
